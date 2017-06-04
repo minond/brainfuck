86b2965be3020bd5f146da6644345c37f4519076
Marcos Minond <minond.marcos@gmail.com>
2017-06-04 17:57:01 -0600

    moving editor to choo

--- src/vizualizer/editor.js
+++ src/vizualizer/editor.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const EV_UPDATE_PROG = 'updateprogram'
+
 const brainfuck = require('../interpreter/js/brainfuck')
 const html = require('choo/html')
 const choo = require('choo')
@@ -9,27 +11,32 @@ const helloworld = require('../bf/helloworld.bf')
 const app = choo()
 
 app.use(logger)
-app.route('*', editor)
+app.use(controls)
+
+app.route('*', editor_view)
 app.mount('#view')
 
-function editor() {
+function editor_view(state, emit) {
+  const run = () =>
+    console.log('runnig')
+
   return html`
     <section class="pa3 pa5-ns cf">
       <h1 class="f3 f-headline-m tipitop">Brainfuck</h1>
 
       <div class="editor-section fl w-100 w-50-ns bg-near-white tc">
-        <button id="run" class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black">Run</button>
-        <button id="pause" class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black">Pause</button>
-        <button id="step" class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black">Step</button>
+        ${editor_button('Run', { onclick: run })}
+        ${editor_button('Pause')}
+        ${editor_button('Step')}
 
-        <div id="editor" contenteditable="true" class="editor">${helloworld}</div>
+        ${editor(state, emit)}
       </div>
 
       <div class="editor-section fl w-100 w-50-ns bg-near-white tc">
         <!-- these are here just for spacing... -->
-        <button class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black hidden">&nbsp;</button>
-        <button class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black hidden">&nbsp;</button>
-        <button class="editor-ctrl f6 link dim ba ph3 pv2 mb2 dib black hidden">&nbsp;</button>
+        ${editor_button('.', '', 'hidden')}
+        ${editor_button('.', '', 'hidden')}
+        ${editor_button('.', '', 'hidden')}
 
         <div contenteditable="true" class="editor"></div>
       </div>
@@ -39,7 +46,39 @@ function editor() {
   `
 }
 
+function controls(state, emitter) {
+  state.program = helloworld
+
+  emitter.on(EV_UPDATE_PROG, (prog) => {
+    state.program = prog
+    emitter.emit('render')
+  })
+}
+
 function logger(state, emitter) {
   emitter.on('*', (...args) =>
     console.log('%cchoochoo', 'color: #346fff', ...args))
+}
+
+function editor(state, emit) {
+  const update_program = (prog) =>
+    emit(EV_UPDATE_PROG, prog)
+
+  const elem = html`<div contenteditable="true" class="editor"
+    oninput=${(e) => update_program(e.target.innerText)}>${state.program}</div>`
+
+  elem.isSameNode = (target) =>
+    state.program === target.innerText
+
+  return elem
+}
+
+function editor_button(value, attrs = '', extra_classes = '') {
+  return button(value, attrs, `editor-ctrl ${extra_classes}`)
+}
+
+function button(value, attrs = '', extra_classes = '') {
+  return html`<button ${attrs} class="${extra_classes} f6 link dim ba ph3 pv2 mb2 dib black">
+    ${value}
+  </button>`
 }
